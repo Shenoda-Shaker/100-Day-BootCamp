5ada1b5c290c43beb53706d695f702dd39df04d8
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-03 00:51:18 +0300

    add function

--- day_01/main.py
+++ day_01/main.py
@@ -1,3 +1,11 @@
-print ("Hello Maryam - Charlie - Shenoda")
+# print ("Hello Maryam - Charlie - Shenoda")
+#
+# print (" done, the file uploaded")
 
-print (" done, the file uploaded")
+def family(name,age):
+    return
+
+person_name = input("enter your name")
+person_age = input("enter your age")
+
+print (person_name,person_age)
