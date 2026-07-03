1b4f44f57bcc9702557510aba005f203c725c30e
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-03 17:03:09 +0300

    add len function

--- day_01/variable.py
+++ day_01/variable.py
@@ -1,15 +1,29 @@
-# -------------------------- strings manipulation --------------------------
+# # -------------------------- strings manipulation --------------------------
+#
+# print ("I love \nMaryam & \nCharlie")
+#
+# print ("I love Roma \tand \tcharlie")
+#
+# # -------------------------- Combine two strings --------------------------
+#
+# print ("My Family is " + "Maryam & Charlie")
+#
+# # -------------------------- input function --------------------------
+#
+# print ("Hello ya " + input("please enter your name: ") + " !")
+#
+# print ("this update from git online")
 
-print ("I love \nMaryam & \nCharlie")
+# -------------------------- Variables  --------------------------
 
-print ("I love Roma \tand \tcharlie")
+# user_name = input("What is your name? ")
+# length = " and the length of input is:"
+# print ("Hello Engineer: " + user_name + length,len(user_name))
 
-# -------------------------- Combine two strings --------------------------
+# user_name = input("Enter your name: ")
+# length = len(user_name)
+# print(user_name , "and the length of ch is:",length)
 
-print ("My Family is " + "Maryam & Charlie")
-
-# -------------------------- input function --------------------------
-
-print ("Hello ya " + input("please enter your name: ") + " !")
-
-print ("this update from git online")
+user_name = input ("What is your name? ")
+length =  "and the length of the character is " + str(len(user_name))
+print(user_name, length)
