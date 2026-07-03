28181ba5f3f4ff55041934b24bff3669a8465c28
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-03 19:43:34 +0300

    Day 01 project completed

---
+++ day_01_project.py
@@ -0,0 +1,8 @@
+# -------------------------- Day 1 Project --------------------------
+
+print ("Welcome to the band name generator")
+
+city = input("what's the name of the city you grew up in? ")
+pet_name = input("what's the pet name? ")
+
+print("Your band name could be " + city + " " + pet_name)
