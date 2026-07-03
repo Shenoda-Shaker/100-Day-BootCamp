4d76050fff9f0bbf5fe218402fe12cb73b7a242a
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-03 16:08:25 +0300

    Input Function

--- day_01/variable.py
+++ day_01/variable.py
@@ -1,9 +1,14 @@
-# strings manipulation
+# -------------------------- strings manipulation --------------------------
 
 print ("I love \nMaryam & \nCharlie")
 
 print ("I love Roma \tand \tcharlie")
 
-# Combine two strings:
+# -------------------------- Combine two strings --------------------------
 
 print ("My Family is " + "Maryam & Charlie")
+
+# -------------------------- input function --------------------------
+
+print ("Hello ya " + input("please enter your name: ") + " !")
+
