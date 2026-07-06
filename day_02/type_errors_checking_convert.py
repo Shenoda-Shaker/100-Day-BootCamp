eed21d90ffc39be7e7910a65cccc951d8edcb669
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-06 04:28:43 +0300

    Update

---
+++ day_02/type_errors_checking_convert.py
@@ -0,0 +1,31 @@
+# Type Checking:
+
+# print(type("Roma"))     #String
+#
+# print(type(123))        # Integer
+#
+# print(type(3.14159))    # Float
+#
+# print(type(True))       # Boolean
+
+
+
+# # Data Types Conversion:
+#
+# price = int("123")
+# tip = 4 + price
+#
+# print(tip)
+#
+# print( int("123") + int("456") )
+
+# Quiz & Solution:
+
+# Solution one:
+print ("Number of letters in your name: " + str( len(input("Enter the name: "))))
+
+# Solution Two:
+
+name_of_user = input("Enter your name: ")
+length = (len(name_of_user))
+print ("Number of letters in your name: " + str(length))
