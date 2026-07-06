7a2d8c153f963faac37d4bf33221c8e522879f8e
Shenoda-Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-06 03:23:13 +0300

    Basic data types

---
+++ day_02/data_types.py
@@ -0,0 +1,24 @@
+#Strings &  Subscripting:
+
+# Access an item inside a group using []
+# Example for string:
+# M	A	R	Y	A	M
+# 0	1	3	3	4	5
+print ("Maryam"[0])	#Result will be M
+
+#Example for list
+Family_members = ["Shenoda" ,"Charlie", "Maryam"]
+print (Family_members[1])	#Result will be Charlie
+
+# Integer : Whole Numbers:
+print( 123 + 345)
+print(123_456_789_101112)   # _ to makes a large number readable,ust between numbers only
+
+
+# Floasting:
+print(3.14159)
+
+
+#Boolean:
+print(True)
+print(False)
