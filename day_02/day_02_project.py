d2b676ac7f4d95e79ee98b4640b3be363d8f9e09
Shenoda Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-08 02:21:03 +0300

    Complete the day two project :)

--- day_02/day_02_project.py
+++ day_02/day_02_project.py
@@ -5,5 +5,5 @@ total_bill = float(input("What was the total bill? "))
 tip = float(input("How much tip would you like to give? 10, 12, or 15 "))
 split_bill = int(input("How many people to split the bill? "))
 tip_amount = (total_bill * tip) / 100
-pay = ( total_bill + tip_amount ) / split_bill
-print("Each person should pay: ", pay)
+final_amount = ( total_bill + tip_amount ) / split_bill
+print(f"Each person should pay ${final_amount:.2f}")
