16deb6def146d62982035fe35716e3a826b0db59
Shenoda Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-11 23:25:43 +0300

    odd / eve challenge

--- day_03/conditional_stat.py
+++ day_03/conditional_stat.py
@@ -5,8 +5,24 @@
 # else:
 #     do this
 
-height = float(input("What is the height in centimeters? "))
-if height > 120:
-    print(f"your height is : ({height}) so you can ride")
+# height = round(float(input("What is the height in centimeters? "))) # if 119.5 wll consider the height = 120, because round it.
+# if height >= 120:
+#     print(f"your height is : ({height}) so you can ride")
+# else:
+#     print(f"your height is :({height}) so you can't ride")
+
+# Modulo operator
+
+# print ( 10 % 2)
+# print ( 10 % 3)
+
+
+
+# Even or Odd Number challenge:
+
+is_number = int(input("Please enter the number: "))
+if is_number % 2 == 0:
+    print(f"The number you had enter is {is_number} is EVEN, because the reminder is {is_number % 2}")
 else:
-    print(f"your height is :({height}) so you can't ride")
+    print(f"The number you had enter is {is_number} is ODD, because the reminder is {is_number % 2}")
+
