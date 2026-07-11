e2d100d560c9441131a574a5bffe12652f1e6e5a
Shenoda Shaker <eng_shenoda_shaker@hotmail.com>
2026-07-11 22:13:31 +0300

    basic if/else

---
+++ day_03/conditional_stat.py
@@ -0,0 +1,12 @@
+# Conditional Statements, Logical, Blocks, Scope:
+# if / els syntax
+# if condition:
+#     do this
+# else:
+#     do this
+
+height = float(input("What is the height in centimeters? "))
+if height > 120:
+    print(f"yor height is : ({height}) so you can ride")
+else:
+    print(f"yor height is :({height}) so you can't ride")
